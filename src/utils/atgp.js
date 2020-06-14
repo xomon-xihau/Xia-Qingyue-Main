84f55c21e795d168806169672061132694459c45
Xomon Xihau <57447181+xomon-xihau@users.noreply.github.com>
2020-06-14 17:35:36 +0530

    Update atgp.js

--- src/utils/atgp.js
+++ src/utils/atgp.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = {
   cz: ['https://i.imgur.com/PDKa74q.jpg'],
-  yc: ['https://i.imgur.com/FtOzzhU.png', 'https://i.imgur.com/Teqt0Az.png', 'https://i.imgur.com/4Mu6Psy.jpg', 'https://i.imgur.com/8gx6oqJ.jpg', 'https://i.imgur.com/lwrdfPk.jpg', 'https://i.imgur.com/YXg8LLB.png', 'https://i.imgur.com/qG0b3DG.jpg', 'https://media.discordapp.net/attachments/700230031527379024/719149526820323368/1fb0bbba5a9a076f.jpg', 'https://media.discordapp.net/attachments/700230031527379024/719149507555885106/29354c675cc1becb.jpg'],
-  js: ['https://i.imgur.com/95mMTHp.jpg', 'https://i.imgur.com/2kzBHyZ.jpg', 'https://i.imgur.com/4Mu6Psy.jpg', 'https://i.imgur.com/Einzxqi.jpg', 'https://i.imgur.com/1qPFptr.jpg', 'https://media.discordapp.net/attachments/700230031527379024/707890731242750012/8-.jpg', 'https://media.discordapp.net/attachments/700230031527379024/707890714943422514/-.jpg'],
+  yc: ['https://i.imgur.com/FtOzzhU.png', 'https://i.imgur.com/Teqt0Az.png', 'https://i.imgur.com/4Mu6Psy.jpg', 'https://i.imgur.com/8gx6oqJ.jpg', 'https://i.imgur.com/lwrdfPk.jpg', 'https://i.imgur.com/YXg8LLB.png', 'https://i.imgur.com/qG0b3DG.jpg', 'https://i.imgur.com/RRzaTjv.jpg', 'https://i.imgur.com/pMyv07W.jpg'],
+  js: ['https://i.imgur.com/95mMTHp.jpg', 'https://i.imgur.com/2kzBHyZ.jpg', 'https://i.imgur.com/4Mu6Psy.jpg', 'https://i.imgur.com/Einzxqi.jpg', 'https://i.imgur.com/1qPFptr.jpg', 'https://i.imgur.com/GfmLr9C.jpg', 'https://i.imgur.com/HDfsg0y.jpg'],
   mx: ['https://i.imgur.com/1TfcGq4.png', 'https://i.imgur.com/GwaYI8E.jpg'],
   cw: ['https://i.imgur.com/9lQjD1i.png', 'https://i.imgur.com/7L7pKaS.png', 'https://i.imgur.com/SE3DO2W.png'],
   cyc: ['https://i.imgur.com/Xcht5Gn.jpg', 'https://i.imgur.com/BCdYBIV.png', 'https://i.imgur.com/pmFpQq3.jpg'],
